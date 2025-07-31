94f71d58630cf8b14376180050f1fa52def47720
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2025-07-31 12:21:27 +0300

    Temporary Fix

--- Assets/Scripts/MainMenu/ShopCharacterUI.cs
+++ Assets/Scripts/MainMenu/ShopCharacterUI.cs
@@ -17,8 +17,10 @@ public class ShopCharacterUI : MonoBehaviour
         shopCharacter = character;
         characterPreview.texture = character.character.characterRenderTexture;
         characterNameText.text = character.character.characterName;
-        priceText.text = $"${character.price:F0}";
-        statusText.text = character.character.isLocked ? "Locked" : "Unlocked";
+        if(priceText != null)
+            priceText.text = $"${character.price:F0}";
+        if(statusText != null)
+            statusText.text = character.character.isLocked ? "Locked" : "Unlocked";
         buyButton.interactable = character.character.isLocked && PlayerInventory.Instance.GetCurrency() >= character.price;
         buyButton.onClick.RemoveAllListeners();
         buyButton.onClick.AddListener(OnBuyCharacterClicked);
